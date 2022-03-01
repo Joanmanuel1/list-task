20d9d818a0579dc9ef84f4145c66e6284f8e7426
adogtame <83562485+adogtame@users.noreply.github.com>
2022-03-01 05:59:32 -0300

    ocultar

--- task-list/src/app/components/add-task/add-task.component.ts
+++ task-list/src/app/components/add-task/add-task.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import {Task} from '../../Task'
+import { UiService } from 'src/app/service/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-add-task',
@@ -14,9 +16,15 @@ export class AddTaskComponent implements OnInit {
   text: string = "";
   day: string = "";
   reminder: boolean = false;
+  subscription?: Subscription;
+  showAddTask: boolean = false;
 
 
-  constructor() { }
+  constructor(
+    private uiService: UiService
+  ) { 
+    this.subscription = this.uiService.onToggle().subscribe(value => this.showAddTask = value)
+  }
 
   ngOnInit(): void {
 

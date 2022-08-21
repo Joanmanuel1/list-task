fd9900e1f88e55466902f9754ea5beaab6d37b66
joanmanuel1 <joanmanuelromero100@gmail.com>
2022-08-21 02:34:34 -0300

    firebase

--- src/app/service/usuario.service.ts
+++ src/app/service/usuario.service.ts
@@ -9,8 +9,8 @@ import { Experiencia, Usuario } from '../model/usuarioModel';
 	providedIn: 'root'
 })
 export class UsuarioService {
-	 //API_URI = 'https://portfolio-arg-programa.herokuapp.com'; 
-	 API_URI = 'http://localhost:8080';
+	 API_URI = 'https://portfolio-arg-programa.herokuapp.com'; 
+	 //API_URI = 'http://localhost:8080';
 
 	constructor(private http: HttpClient, private router: Router) { }
 
